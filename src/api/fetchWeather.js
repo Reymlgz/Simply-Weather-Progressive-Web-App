fcc45512fe719894ec8270a6f10c2273e3c1930a
Rey molina <reymolinagonz@gmail.com>
2023-12-21 13:25:49 -0300

    Updates

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-const API_KEY = 'ab55902b9e2cab55bde987cf82c0b6f5' //Create a new one with a free account on https://api.openweathermap.org,
+const API_KEY = '' //Create a new one with a free account on https://api.openweathermap.org,
 
 export const fetchWeather = async (query) => {
     const {data} = await axios.get(URL, {
